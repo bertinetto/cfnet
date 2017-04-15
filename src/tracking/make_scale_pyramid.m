27998a743079a26e8d1c999f3309bbddddf87e23
Luca Bertinetto <luca.bertinetto@gmail.com>
2017-04-15 14:22:42 +0100

    mergefactor left in

--- src/tracking/make_scale_pyramid.m
+++ src/tracking/make_scale_pyramid.m
@@ -9,22 +9,14 @@ function [pyramid, pad_masks_x] = make_scale_pyramid(im, targetPosition, in_side
     max_target_side = in_side_scaled(end);
     min_target_side = in_side_scaled(1);
     search_side = round(out_side * max_target_side / min_target_side);
-    if p.merge_factor > 0
-        [search_region, ~, pad_mask_region] = get_subwindow_mask(im, targetPosition, [search_side search_side], [max_target_side max_target_side], avgChans, p.gpus, []);
-    else
-       [search_region, ~] = get_subwindow_tracking(im, targetPosition, [search_side search_side], [max_target_side max_target_side], avgChans, p.gpus);
-    end
-    if p.subMean
+    [search_region, ~] = get_subwindow_tracking(im, targetPosition, [search_side search_side], [max_target_side max_target_side], avgChans, p.gpus);
+    
+	if p.subMean
         search_region = bsxfun(@minus, search_region, reshape(stats.x.rgbMean, [1 1 3]));
     end
     
     for s = 1:n
         target_side = round(out_side * in_side_scaled(s) / min_target_side);
-        if p.merge_factor > 0            
-            [pyramid(:,:,:,s), ~, pad_masks_x(:,:,:,s)] = get_subwindow_mask(search_region, (1+search_side*[1 1])/2, [out_side out_side], target_side*[1 1], avgChans, p.gpus, pad_mask_region);
-        else
-            pyramid(:,:,:,s) = get_subwindow_tracking(search_region, (1+search_side*[1 1])/2, [out_side out_side], target_side*[1 1], avgChans, p.gpus);
-        end
-
+        pyramid(:,:,:,s) = get_subwindow_tracking(search_region, (1+search_side*[1 1])/2, [out_side out_side], target_side*[1 1], avgChans, p.gpus);
     end
 end
